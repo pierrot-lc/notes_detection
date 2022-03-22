401a4fa0758000b0d6dd2a31174db0e3f51eb028
PierrotLC <pstmrtem@outlook.com>
2022-03-22 19:42:06 +0100

    feat: detection postprocess OK

--- src/detection.py
+++ src/detection.py
@@ -37,19 +37,26 @@ def predict_labels(
     return labels
 
 
-def fill_blanks(labels: np.ndarray, delta: int):
+def fill_blanks(labels: np.ndarray, delta: int) -> np.ndarray:
     """Add missing pitchs when a serie of the same pitch is predicted,
     with some noisy small blanks between them.
     The parameter delta is setting how much long a blank can be.
+
+    This algorithm has time complexity of O(n).
 
     Input
     -----
         - labels: One-hot pitches for all samples.
             Shape of [n_samples, pitches].
         - delta: Number of consecutive blanks accepted to get filled.
+
+    Output
+    ------
+        - filled: One-hot pitches for all samples, where 0's are replaced
+            by 1's if they are no more than `delta` consecutives.
+            Shape of [n_samples, pitches].
     """
     labels = 1 - labels
-    # print('Reversed:\t', *labels)
 
     curr_sum = np.zeros(labels.shape[1], dtype=int)
     for sample_id, sample in enumerate(labels):
@@ -58,9 +65,7 @@ def fill_blanks(labels: np.ndarray, delta: int):
 
         labels[sample_id] = curr_sum
 
-    # print('Cumulated:\t', *labels)
     labels = np.flip(labels, axis=0)  # Take the samples in reverse order
-    # print('Flipped:\t', *labels)
     between_notes = np.zeros(labels.shape[1], dtype=int)  # Wether we are between pressed pitches or not
     for sample_id, sample in enumerate(labels):
         between_notes[sample == 0] = 1
@@ -69,6 +74,34 @@ def fill_blanks(labels: np.ndarray, delta: int):
 
     labels = np.flip(labels, axis=0)  # Go back to the original order
 
+    return labels
+
+
+def postprocess(
+        labels: np.ndarray,
+        max_zeros: int,
+        max_ones: int
+    ) -> np.ndarray:
+    """Fill low consecutives 0's by 1's, and remove low consecutives 1's by 0's.
+    Low consecutives 0's are considered as false negative and low consecutives 1's
+    are considered as false positive.
+
+    Input
+    -----
+        - labels: One-hot pitches for all samples.
+            Shape of [n_samples, pitches].
+        - max_zeros: Number of consecutive 0's accepted to get filled.
+        - max_ones: Number of consecutive 1's accepted to get replaced.
+
+    Output
+    ------
+        - labels: One-hot pitches postprocessed.
+            Shape of [n_samples, pitches].
+    """
+    labels = fill_blanks(labels, max_zeros)
+    labels = 1 - labels
+    labels = fill_blanks(labels, max_ones)
+    labels = 1 - labels
     return labels
 
 
@@ -88,8 +121,9 @@ if __name__ == '__main__':
         [1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
         [1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1],
     ]).transpose(1, 0)
-    filled = fill_blanks(labels, 2)
+    filled = postprocess(labels, max_zeros=1, max_ones=2)
 
+    print('For max_zeros = 1 and max_ones = 2')
     for original, final in zip(
         labels.transpose(1, 0),
         filled.transpose(1, 0),
