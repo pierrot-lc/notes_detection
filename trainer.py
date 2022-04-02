3e7921137ccc274223a1f9d4e23cd1228d73bcb4
PierrotLC <pstmrtem@outlook.com>
2022-04-02 19:10:57 -0400

    feat: now a summary of the hyperparameters before training

--- trainer.py
+++ trainer.py
@@ -1,11 +1,13 @@
 """Train a model based on config files.
 """
+import sys
 import wandb
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torchinfo import summary
 
 from src.train import train
 from src.data import load, get_stats, AMTDataset
@@ -13,7 +15,7 @@ from src.mlp import AMTMLP
 from src.cnn import AMTCNN
 
 
-def init_config() -> dict:
+def init_config(model_type: str) -> dict:
     config = {
         'group': 'Pitch prediction',
         'device': 'cuda' if torch.cuda.is_available() else 'cpu',
@@ -26,7 +28,7 @@ def init_config() -> dict:
         'n_samples_by_item': 100,
         'lr': 1e-4,
         'pos_weight': 10,
-        'model_type': 'CNN',
+        'model_type': model_type,
     }
 
     return config
@@ -117,9 +119,49 @@ def load_config(config: dict):
     )
 
 
+def print_infos(config: dict):
+    """Show the config file informations.
+    Also print the model summary.
+    """
+    ignore = {'model', 'optimizer', 'train_loader', 'test_loader'}
+    tabsize = 30
+
+    print(f'{2 * tabsize * "-"} Notes Predictions {2 * tabsize * "-"}\n\n')
+
+    for param, value in config.items():
+        if param in ignore:
+            continue
+
+        param_exp = f'[{param}]\t'.expandtabs(tabsize)
+        print(f'     {param_exp}-\t\t{value}')
+
+    print('')
+
+    summary(
+        config['model'],
+        input_size=(
+            config['batch_size'] * config['n_samples_by_item'],
+            config['window_size'],
+        ),
+        depth=2,
+    )
+
+
 if __name__ == '__main__':
-    config = init_config()
+    available_types = ['MLP', 'CNN']
+    if len(sys.argv) != 2:
+        print(f'Usage: {sys.argv[0]} [model_type]')
+        print('Available model types:', *available_types)
+        sys.exit(0)
+
+    model_type = sys.argv[1]
+    config = init_config(model_type)
     load_config(config)
+    print_infos(config)
+
+    continue_training = input('\n\nContinue? [y/n] ')
+    if continue_training != 'y':
+        sys.exit(0)
 
     with wandb.init(
         project='Automatic Music Transcription',
