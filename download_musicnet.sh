eed38a35cbe2021e90012f197db6b35e0807697f
Pierrot LC <pstmrtem@outlook.com>
2022-11-20 11:49:26 +0100

    feat: downloading script

---
+++ download_musicnet.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p data
+cd data
+
+curl https://zenodo.org/record/5120004/files/musicnet_midis.tar.gz -o musicnet_midis.tar.gz
+curl https://zenodo.org/record/5120004/files/musicnet_metadata.csv -o musicnet_metadata.csv
+curl --parallel https://zenodo.org/record/5120004/files/musicnet.tar.gz -o musicnet.tar.gz
+
+tar -xf musicnet_midis.tar.gz
+tar -xf musicnet.tar.gz
