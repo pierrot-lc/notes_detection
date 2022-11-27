d93a31169ea686c87b239c4d9a8ae8ee74ab2d48
Pierrot LeCon <pstmrtem@outlook.com>
2022-11-27 19:29:03 +0100

    fix: exit on failed cd

--- download_musicnet.sh
+++ download_musicnet.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p data
-cd data
+cd data  || exit  # Exit if `cd` fails.
 
 curl https://zenodo.org/record/5120004/files/musicnet_midis.tar.gz -o musicnet_midis.tar.gz
 curl https://zenodo.org/record/5120004/files/musicnet_metadata.csv -o musicnet_metadata.csv
