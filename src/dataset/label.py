6916089388acb06fa40409b22a98b9a1ea33049c
Pierrot LeCon <pstmrtem@outlook.com>
2022-11-27 19:31:33 +0100

    todo: test label dataset

--- src/dataset/label.py
+++ src/dataset/label.py
@@ -5,7 +5,7 @@ import pandas as pd
 class SongLabels:
     """Loader of a song's labels.
 
-    The main method is `from_middle_points` that returns the exact pressed pitches
+    The main method is `from_timesteps` that returns the exact pressed pitches
     for each timsteps given. This method end in `O(n)`, where `n` is the number of
     events (keys pressed/released) in the song.
 
@@ -34,23 +34,24 @@ class SongLabels:
             self.events.append((end, note, instrument, False))
 
         self.events = list(sorted(self.events, key=lambda el: el[0]))  # Sort by time.
+        self.events.insert(0, (0, 1, 1, False))  # Add base event.
 
-    def from_middle_points(self, middle_points: np.ndarray) -> np.ndarray:
+    def from_timesteps(self, timesteps: np.ndarray) -> np.ndarray:
         """Return the labels for each points given in that song.
         This is done in `O(n)` time, where `n` is `len(self)`.
 
-        Input
-        -----
-            middle_points: Array of timesteps.
+        Args
+        ----
+            timesteps: Array of timesteps. *They should be sorted!*
                 Shape of [n_points,].
 
-        Output
-        ------
+        Returns
+        -------
             labels: One-hot vectors for each timesteps.
                 Shape of [n_points, n_instruments, n_pitches].
         """
         labels = np.zeros(
-            (len(middle_points), self.n_instruments, self.n_pitches), dtype=np.uint8
+            (len(timesteps), self.n_instruments, self.n_pitches), dtype=np.uint8
         )
 
         point_id = 0
@@ -61,21 +62,20 @@ class SongLabels:
         try:
             # We go through all events and fill the labels until
             # all are processed or events are terminated.
-            while point_id < len(middle_points):
-                if curr_time >= middle_points[point_id]:
+            while point_id < len(timesteps):
+                if curr_time >= timesteps[point_id]:
                     labels[point_id] = previous_labels
                     point_id += 1
                 else:
                     previous_labels = curr_labels
                     curr_labels, curr_time = next(iter_label)
         except StopIteration:
-            # The last middle points are after the end of the song,
+            # The last points are after the end of the song,
             # so we assume the end of the song is containing no labels.
-            while point_id < len(middle_points):
-                labels[point_id] = np.zeros(
-                    (self.n_instruments, self.n_pitches), dtype=int
-                )
-                point_id += 1
+            labels[point_id:] = np.zeros(
+                (len(timesteps) - point_id, self.n_instruments, self.n_pitches),
+                dtype=int,
+            )
 
         return labels
 
@@ -88,13 +88,12 @@ class SongLabels:
 
     def __iter__(self):
         """Returns itself.
-        It initializes some variables for a properly fresh iteration through all events.
+        It initializes some variables for a fresh iteration through all events.
         """
         self.curr_index = 0
         self.curr_labels = np.zeros(
             (self.n_instruments, self.n_pitches), dtype=np.uint8
         )
-        self.first_iter = True
         return self
 
     def __next__(self) -> tuple[np.ndarray, int]:
@@ -103,32 +102,25 @@ class SongLabels:
         If some events occurs at the same time, they will be gathered into one
         iteration.
 
-        Output
-        -----
+        Returns
+        -------
             label: One-hot vector for the current time step.
                 Shape of [n_instruments, n_pitches].
-            time: Time step corresponding to this label. This time step is
-                rescaled to take into account the `downsampling_factor`.
+            time: Time step corresponding to this label.
         """
         if self.curr_index >= len(self):
             raise StopIteration
 
-        if self.first_iter:  # Return the labels for the first iteration
-            self.first_iter = False
-            return self.curr_labels.copy(), 0
+        time, note, instrument, is_pressed = self.events[self.curr_index]
+        self.curr_labels[instrument - 1, note - 1] = int(is_pressed)
+        self.curr_index += 1
 
-        # Do update the labels while the time event does not change
-        first_iter = True  # Do-while loop
-        while first_iter or (
-            self.curr_index < len(self)
-            and (
-                self.events[self.curr_index - 1][0] == self.events[self.curr_index][0]
-            )  # Is the current event at the same time as the preceding one?
-        ):
+        # Do update the labels while the time event does not change.
+        while self.curr_index < len(self) and (
+            self.events[self.curr_index - 1][0] == self.events[self.curr_index][0]
+        ):  # Is the current event at the same time as the preceding one?
             time, note, instrument, is_pressed = self.events[self.curr_index]
             self.curr_labels[instrument - 1, note - 1] = int(is_pressed)
-
             self.curr_index += 1
-            first_iter = False
 
         return self.curr_labels.copy(), time
---
+++ tests/test_labels.py
@@ -0,0 +1,16 @@
+import pytest
+
+# @pytest.mark.parametrize(
+#    'input_name, output_name',
+#     [
+#         ('issu du croisement (1 croisement possible)', 'issu du croisement'),
+#         ('issu du croisement (10 croisements possibles)', 'issu du croisement'),
+#         ('issus du croisement', 'issus du croisement'),
+#         ('issu du croisements', 'issu du croisements'),
+#     ]
+# )
+# def test_labels
+
+# TODO:
+# * Test `from_timesteps` (basecase, extreme cases)
+# * Validate the overall format (len, event format)
